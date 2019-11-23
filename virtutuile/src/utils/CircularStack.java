888a79faed595689a735950178ef5ab8c7605523
Francois <francois_levasseur@hotmail.com>
2019-11-23 14:54:07 -0500

    added circular stack

---
+++ virtutuile/src/utils/CircularStack.java
@@ -0,0 +1,32 @@
+package utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CircularStack<T> {
+
+    private List<T> array;
+    private int N;
+
+    public CircularStack(int N) {
+        this.N = N;
+        array = new ArrayList<T>(N);
+    }
+
+    public void add(T newObject) {
+        if (array.size() < N) {
+            array.add(newObject);
+            return;
+        }
+
+        array.remove(0);
+        array.add(newObject);
+    }
+
+    public T pop() {
+        if (this.array.size() == 0) {
+            return null;
+        }
+        return array.remove(array.size() - 1);
+    }
+}

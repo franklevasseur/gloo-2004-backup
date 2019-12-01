304e0f96d39dd1f6a498c03e5438d1faa1808d2b
Arnaud Dorval <arnaud.dorval-leblanc.1@ulaval.ca>
2019-12-01 15:48:23 -0500

    Calculer area d'une surface dans accounting

--- virtutuile/src/Domain/accounting.java
+++ virtutuile/src/Domain/accounting.java
@@ -1,5 +1,7 @@
 package Domain;
 
+import utils.Point;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -43,5 +45,52 @@ public class accounting {
             }
         }
         return usedTiles;
+    }
+
+    private double TotalArea(){
+        for ( Surface i: surfaces){
+            //check si la surface est une surface fusionne
+            if(i.isFusionned()){
+
+            }else {
+
+            }
+        }
+        return 2.0;
+    }
+
+    private double areaOfSurface(Surface pSurface){
+
+        return 2.0;
+    }
+
+    private Surface surfaceSansTrou(FusionnedSurface pSurface){
+        Surface trou = null;
+        for ( Surface i: pSurface.getFusionnedSurfaces()){
+            if (i.isHole() == HoleStatus.HOLE){
+                trou = i;
+            }
+        }
+        if(trou == null){
+            //return pSurface;
+        }else {
+/**
+            for(Point j: trou){
+                for (Point k: pSurface.getFusionnedSurfaces() ) {
+
+                    if (...) {
+                        iter.remove();
+                    }
+                }
+
+            }
+ */
+        }
+        return pSurface;
+    }
+
+    private Surface surfaceTrou(Surface pSurface){
+
+        return pSurface;
     }
 }

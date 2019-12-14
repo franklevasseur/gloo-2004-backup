2c0f9c9aa85fb604a9774986887e9ab48d7e79a0
Francois <francois_levasseur@hotmail.com>
2019-12-14 12:44:55 -0500

    refactor inspection 2

--- virtutuile/src/Domain/InspectionService.java
+++ virtutuile/src/Domain/InspectionService.java
@@ -2,9 +2,6 @@ package Domain;
 
 import utils.Color;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class InspectionService {
 
     private static InspectionService instance = new InspectionService();
@@ -14,25 +11,20 @@ public class InspectionService {
 
     public String inspect(Project project, double pWidth, double pHeight) {
         int count = 0;
-        for(Surface i: project.getSurfaces()) {
-            count += inspectOneSurface(i, pWidth, pHeight);
+        for(Surface surface: project.getSurfaces()) {
+            count += inspectOneSurface(surface, pWidth, pHeight);
         }
         return String.format("There is %d tiles that do not respect the minimal cut length.", count);
     }
 
     private int inspectOneSurface(Surface surface, double pWidth, double pHeight) {
-        List<Tile> temp = surface.getTiles();
-
         int count = 0;
-        List<Tile> badTiles = new ArrayList<>();
-        for (Tile j: temp){
-            if (j.getWidth() < pWidth || j.getHeight() < pHeight) {
-                badTiles.add(j);
-                j.getMaterial().setColor(Color.RED);
+        for (Tile tile: surface.getTiles()){
+            if (tile.getWidth() < pWidth || tile.getHeight() < pHeight) {
+                tile.getMaterial().setColor(Color.RED);
                 count ++;
             }
         }
-        surface.setTiles(temp);
         return count;
     }
 }

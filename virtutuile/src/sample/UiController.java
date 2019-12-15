3a1a46a829747a1ec2a17c43540936ec8b82a007
Francois <francois_levasseur@hotmail.com>
2019-12-14 20:12:40 -0500

    oupsi :P

--- virtutuile/src/sample/UiController.java
+++ virtutuile/src/sample/UiController.java
@@ -498,7 +498,7 @@ public class UiController implements Initializable {
 //                newSurfaceWidth = Double.valueOf(surfaceWidthInput.toString()).doubleValue();
 
                 CharSequence tileMaterialInput = tileMaterialChoiceBox.getValue();
-                tileMaterialInput = tileAngleInput == null ? "" : tileAngleInput;
+                tileMaterialInput = tileMaterialInput == null ? "" : tileMaterialInput;
 
                 TileDto masterTile = null;
                 if (newMasterTileX != null && newMasterTileY != null && !tileMaterialInput.equals("")) {
